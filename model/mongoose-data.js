17bbdb4442907fe0f35a2014ed0b389e5232c19a
Lassi Ylikojola <lykki2@localhost.localdomain>
2017-03-24 02:14:56 +0200

    Mongoose promise, bluebird and README updates

--- model/mongoose-data.js
+++ model/mongoose-data.js
@@ -1,7 +1,17 @@
-var mongoose = require('mongoose'),
-    db = mongoose.createConnection('localhost', 'euro2012');
-db.on('error', console.error.bind(console, 'connection error:'));
+//See: http://mongoosejs.com/docs/promises.html
+//See: http://stackoverflow.com/questions/35929768/which-is-the-right-way-to-use-bluebird-with-mongoose
+//See: http://stackoverflow.com/questions/40551550/what-is-the-correct-way-of-using-bluebird-for-mongoose-promises
 
+var options = { promiseLibrary: require('bluebird') };
+var mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+db = mongoose.createConnection('localhost', 'euro2012', 27017, options);
+
+//Band = db.model('band-promises', { name: String });
+db.on('error', console.error.bind(console, 'connection error:'));
+//db.on('open', function() {
+//    assert.equal(Band.collection.findOne().constructor, require('bluebird'));
+//});
 
 exports.teamlist = function(gname, callback) {
     db.once('open', function() {
